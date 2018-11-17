be15e093e0c4a6e46c13cd264b1665357ece56b1
Sergey Gerasimov <gerich.home@gmail.com>
2018-11-17 19:46:50 +0000

    Update strings.js

--- testCode/strings.js
+++ testCode/strings.js
@@ -1,3 +1,35 @@
 var a = "abcdefg";
 
 log(a[0] + a[2] + a[5]);
+
+function decimalToHexString(n) {
+    log(n);
+  var hex = "0123456789ABCDEF";
+  n >>>= 0;
+  log(n);
+  var s = "";
+  log('while 1')
+  while (n) {
+      log(n & 0xf)
+      log(hex[n & 0xf])
+    s = hex[n & 0xf] + s;
+    log(s)
+    n >>>= 4;
+    log(n)
+  }
+  log('while 2')
+  while (s.length < 4) {
+    s = "0" + s;
+    log(s)
+  }
+  return s;
+}
+
+function decimalToPercentHexString(n) {
+  var hex = "0123456789ABCDEF";
+  return "%" + hex[(n >> 4) & 0xf] + hex[n & 0xf];
+}
+
+decimalToHexString(100)
+
+log('done')
